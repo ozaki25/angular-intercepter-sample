6c4cab7f80090b5953d0a6e56d354d1b421321f3
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-06-01 12:34:47 +0900

    check response content type

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +10,22 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getHtml(): Observable<HttpResponse<{}>> {
-    return this.http.get('http://localhost:4200/assets/index.html', {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Accept: 'text/html',
-      }),
-      observe: 'response',
-    });
+    return this.http
+      .get('http://localhost:4200/assets/index.html', {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Accept: 'text/html',
+        }),
+        observe: 'response',
+      })
+      .pipe(
+        catchError(res => {
+          if (res.headers.get('content-type').startsWith('text/html')) {
+            alert(res.headers.get('content-type'));
+          }
+          return of(res);
+        }),
+      );
   }
 
   getJson(): Observable<HttpResponse<{}>> {
